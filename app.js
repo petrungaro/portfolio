bf602059e32b0a1c30cc7102676c18cf2730506e
Rob Petrungaro <rob.petrungaro@gmail.com>
2022-06-12 14:45:14 -0400

    added some alt text and tabindex

--- app.js
+++ app.js
@@ -1,47 +1,3 @@
-// ***************************************************************** //
-// ************************** FLOATERS ***************************** //
-const floaters = {};
-
-floaters.iconArray = document.querySelectorAll('.icon');
-floaters.prevX = 0;
-floaters.prevY = 0;
-floaters.moveXAmount = 0;
-floaters.moveYAmount = 0;
-
-
-floaters.init = () => {
-    floaters.attachListener();
-}
-
-floaters.attachListener = () => {
-    const toolsSection = document.querySelector('section.stack');
-    
-    toolsSection.addEventListener("mousemove", function(e) {
-        floaters.getMousePosition(e);
-    })    
-}
-
-floaters.getMousePosition = (e) => {
-    let moveXAmount = e.screenX - floaters.prevX;
-    let moveYAmount = e.screenY - floaters.prevY;
-
-    floaters.moveIcon(moveXAmount, moveYAmount);
-
-    floaters.prevX = e.screenX;
-    floaters.prevY = e.screenY;
-}
-
-floaters.moveIcon = (xAmount, yAmount) => {
-    
-    floaters.iconArray.forEach( (icon) => {
-        let movementStrength = 10 + (Math.random() * 15);
-
-        icon.style.left = (icon.offsetLeft) - (xAmount / movementStrength) + "px";
-        icon.style.top = (icon.offsetTop) - (yAmount / movementStrength) + "px";
-    });
-}
-
-
 // ***************************************************************************** //
 // ********************************PHOTO CHANGER******************************** //
 
@@ -122,6 +78,56 @@ bio.swapPhoto = (object) => {
 }
 
 
+
+// ***************************************************************** //
+// ************************** FLOATERS ***************************** //
+//  https://www.youtube.com/watch?v=ysF0rFjIk7o
+
+
+const floaters = {};
+
+floaters.iconArray = document.querySelectorAll('.icon');
+floaters.prevX = 0;
+floaters.prevY = 0;
+floaters.moveXAmount = 0;
+floaters.moveYAmount = 0;
+
+
+floaters.init = () => {
+    floaters.attachListener();
+}
+
+floaters.attachListener = () => {
+    const toolsSection = document.querySelector('section.stack');
+    
+    toolsSection.addEventListener("mousemove", function(e) {
+        floaters.getMousePosition(e);
+    })    
+}
+
+floaters.getMousePosition = (e) => {
+    let moveXAmount = e.screenX - floaters.prevX;
+    let moveYAmount = e.screenY - floaters.prevY;
+
+    floaters.moveIcon(moveXAmount, moveYAmount);
+
+    floaters.prevX = e.screenX;
+    floaters.prevY = e.screenY;
+}
+
+floaters.moveIcon = (xAmount, yAmount) => {
+    
+    floaters.iconArray.forEach( (icon) => {
+        let movementStrength = 10 + (Math.random() * 15);
+
+        icon.style.left = (icon.offsetLeft) - (xAmount / movementStrength) + "px";
+        icon.style.top = (icon.offsetTop) - (yAmount / movementStrength) + "px";
+    });
+}
+
+
+
+// *********************************************************** //
 // *********************** SOUP CAN ************************** //
 
 const soupcan = {};
@@ -171,11 +177,12 @@ soupcan.showText = (event) => {
 
 
 
+// ! INIT CALLS
 
 document.addEventListener("DOMContentLoaded", function() {
     floaters.init();
     bio.init();
     soupcan.setVariables();
-    console.log("%c Oh, hello there 👋", "color:green; font-size:24px;")
+    console.log("%c Oh, hello there 👋", "color:blue; font-size:20px;")
 });
 
